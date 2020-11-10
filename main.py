0159a8689bd2e4d12353dec922809cdf7ff501f3
SaMuEl23PaJoC23 <sam.alejnadro.sam@gmail.com>
2020-11-10 04:21:51 -0600

    Cambios para subir a heroku

--- main.py
+++ main.py
@@ -368,4 +368,4 @@ def ObtenerPlaylist():
     return (respuesta)
 
 if __name__=="__main__":
-    app.run(port=3000, debug=True)
+    app.run(threaded=True, host="0.0.0.0", port=5000, debug="True")
